3b91b698bf8d584b97b4fa989b921aaf92b00f56
BishalDey908 <ronydey34567@gmail.com>
2024-07-19 20:11:38 +0530

    FONT ADDED TO lANDINGPAGE

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -13,9 +13,9 @@ export function LandingPage() {
     <div onClick={handleButton}>
     <BackgroundGradientAnimation >
       <div className="absolute z-50 inset-0 flex flex-col  items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 font-head font-extrabold">
           <span onClick={handleButton}>Welcome To Melvin</span>
-        <p className="my-10 text-2xl bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">Click anywhare to Explore </p>
+        <p className="my-10 text-xl bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 font-pragraph font-extrabold">Click anywhare to Explore </p>
           
         </p>
       </div>
